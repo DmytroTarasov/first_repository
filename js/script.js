b67617409c0ab20a15f3df898e8937569fcf5e7c
Dmytro <dtarasov24s@gmail.com>
2020-10-15 19:50:25 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+// // a = 5;
+// // console.log(a);
+
+// // let number = 1;
+// // const leftBorderWidth = 1;
+
+// // const num = 1;
+// // const fdhfg = true;
+
+// // const obj = {
+// //     name: "Dima",
+// //     isMarried: false
+// // };
+
+// console.log(obj.name);
+
+// // let arr = ['John', 'Mikhail', 'Dmitrii', 7, {}];
+
+// // const c = BigInt(Number.MAX_SAFE_INTEGER);
+// // const d = c * 2n;
+// // console.log(d);
+
+const result = prompt('How old are you', '');
+console.log(result);
